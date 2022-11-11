658ddbda2e6697db3db1e85d900ab0de27d74c3b
Luis Javier Toro Flores <91231015+LuisToroSck@users.noreply.github.com>
2022-11-11 17:16:39 -0300

    Copia de microservicio en Maven

---
+++ autorizacion-service/src/test/java/com/example/autorizacionservice/AutorizacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autorizacionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
