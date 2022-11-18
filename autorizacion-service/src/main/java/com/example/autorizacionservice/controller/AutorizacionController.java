a23a4391a79b43e7b1fbdc1e5d788c4d49ef0347
Luis Toro <luis.toro.f@usach.cl>
2022-11-18 12:53:47 -0300

    Método actualizar - cambio de post a get

--- autorizacion-service/src/main/java/com/example/autorizacionservice/controller/AutorizacionController.java
+++ autorizacion-service/src/main/java/com/example/autorizacionservice/controller/AutorizacionController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.*;
@@ -44,7 +45,7 @@ public class AutorizacionController {
         return ResponseEntity.ok(marcasReloj);
     }
 
-    @PostMapping("/actualizar/{id}")
+    @GetMapping("/actualizar/{id}")
     public ResponseEntity<AutorizacionEntity> actualizarAutorizaciones(@PathVariable Long id){
         AutorizacionEntity autorizacion = autorizacionRepository.getById(id);
         System.out.println("estado: "+autorizacion.getAutorizado());
@@ -55,6 +56,8 @@ public class AutorizacionController {
         }
         return ResponseEntity.ok(autorizacion);
     }
+
+ 
 
     @PostMapping("/eliminar")
     public ResponseEntity<List<AutorizacionEntity>> eliminarAutorizaciones(){
