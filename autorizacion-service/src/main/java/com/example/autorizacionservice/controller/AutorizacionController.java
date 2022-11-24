db50b6f8f4e80c969a56bcce514e72fb587c156b
LuisToroSck <luis.toro.f@usach.cl>
2022-11-23 23:17:06 -0300

    Configuración local

--- autorizacion-service/src/main/java/com/example/autorizacionservice/controller/AutorizacionController.java
+++ autorizacion-service/src/main/java/com/example/autorizacionservice/controller/AutorizacionController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3001")
+//@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 @RequestMapping("/autorizacion")
 public class AutorizacionController {
